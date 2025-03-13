da9a16405947108b0c172337cb478001d142e443
devinschumacher <devin@serp.co>
2025-03-13 21:24:08 +0900

    WIP

--- packages/auth/pages/signup.vue
+++ packages/auth/pages/signup.vue
@@ -1,63 +1,20 @@
 <template>
   <div>
-    <div v-if="error" class="error">{{ error }}</div>
-
-    <ULabel>Name</ULabel>
-    <UInput v-model="name" placeholder="Your full name" />
-
-    <ULabel>Email</ULabel>
-    <UInput v-model="email" placeholder="you@example.com" />
-
-    <ULabel>Username</ULabel>
-    <UInput v-model="username" placeholder="Username" />
-
-    <ULabel>Password</ULabel>
-    <UInput v-model="password" type="password" placeholder="Your password" />
-
-    <UButton :disabled="loading" @click="handleSignup"> Sign Up </UButton>
+    <UButton
+             v-for="provider in providers"
+             :key="provider.id"
+             @click="signIn(provider.id)">
+      Sign in with {{ provider.name }}
+    </UButton>
   </div>
 </template>
 
 <script setup lang="ts">
+// Remember to disable the middleware protection from your page!
 definePageMeta({
-  middleware: 'sidebase-auth',
   auth: { unauthenticatedOnly: true, navigateAuthenticatedTo: '/' }
 });
 
-const { signIn } = useAuth();
-
-const name = ref('');
-const email = ref('');
-const username = ref('');
-const password = ref('');
-
-const error = ref('');
-const loading = ref(false);
-
-async function handleSignup() {
-  error.value = '';
-  loading.value = true;
-  try {
-    await $fetch('/api/register', {
-      method: 'POST',
-      body: {
-        name: name.value,
-        email: email.value,
-        username: username.value,
-        password: password.value
-      }
-    });
-
-    // Sign in the user after successful registration.
-    await signIn('credentials', {
-      username: username.value,
-      password: password.value,
-      callbackUrl: '/'
-    });
-  } catch (err) {
-    error.value = err.data?.error || 'Something went wrong!';
-  } finally {
-    loading.value = false;
-  }
-}
+const { signIn, getProviders } = useAuth();
+const providers = await getProviders();
 </script>
--- packages/auth/server/api/auth/[...].ts
+++ packages/auth/server/api/auth/[...].ts
@@ -3,42 +3,61 @@ import { NuxtAuthHandler } from '#auth';
 import { users } from '@serp/auth/server/api/dbAuth/schema';
 import { authDb } from '@serp/auth/server/api/dbAuth';
 import { eq } from 'drizzle-orm';
-import bcrypt from 'bcrypt';
+import GithubProvider from 'next-auth/providers/github';
 
 export default NuxtAuthHandler({
   secret: process.env.AUTH_SECRET,
+
   providers: [
     // @ts-expect-error Use .default here for it to work during SSR.
-    CredentialsProvider.default({
-      name: 'Credentials',
-      credentials: {
-        username: { label: 'Username', type: 'text', placeholder: 'jsmith' },
-        password: { label: 'Password', type: 'password' }
-      },
-      async authorize(credentials, req) {
-        // Fetch user by username
-        const results = await authDb
-          .select()
-          .from(users)
-          .where(eq(users.username, credentials.username))
-          .limit(1)
-          .execute();
-
-        if (results.length) {
-          const user = results[0];
-
-          // Compare provided password with stored hash
-          const isValid = await bcrypt.compare(
-            credentials.password,
-            user.password
-          );
-          if (isValid) {
-            return user;
-          }
-        }
-        // Return null if user data could not be retrieved
-        return null;
-      }
+    GithubProvider.default({
+      clientId: process.env.AUTH_GITHUB_CLIENT_ID,
+      clientSecret: process.env.AUTH_GITHUB_CLIENT_SECRET
     })
+    // @ts-expect-error Use .default here for it to work during SSR.
+    // RedditProvider.default({
+    //   clientId: process.env.REDDIT_CLIENT_ID,
+    //   clientSecret: process.env.REDDIT_CLIENT_SECRET
+    // }),
+    // @ts-expect-error Use .default here for it to work during SSR.
+    // GoogleProvider.default({
+    //   clientId: process.env.GOOGLE_CLIENT_ID,
+    //   clientSecret: process.env.GOOGLE_CLIENT_SECRET
+    // });
   ]
+
+  // providers: [
+  // @ts-expect-error Use .default here for it to work during SSR.
+  // CredentialsProvider.default({
+  //   name: 'Credentials',
+  //   credentials: {
+  //     username: { label: 'Username', type: 'text', placeholder: 'jsmith' },
+  //     password: { label: 'Password', type: 'password' }
+  //   },
+  //   async authorize(credentials, req) {
+  //     // Fetch user by username
+  //     const results = await authDb
+  //       .select()
+  //       .from(users)
+  //       .where(eq(users.username, credentials.username))
+  //       .limit(1)
+  //       .execute();
+
+  //     if (results.length) {
+  //       const user = results[0];
+
+  //       // Compare provided password with stored hash
+  //       const isValid = await bcrypt.compare(
+  //         credentials.password,
+  //         user.password
+  //       );
+  //       if (isValid) {
+  //         return user;
+  //       }
+  //     }
+  //     // Return null if user data could not be retrieved
+  //     return null;
+  //   }
+  // })
+  // ]
 });
