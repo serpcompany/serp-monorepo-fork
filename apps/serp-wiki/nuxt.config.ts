dd75806bd78b5f8fe2033b8e98acb9a02108524c
Francis LaBounty <73464335+francislabountyjr@users.noreply.github.com>
2025-03-18 13:05:14 -0600

    transpile next-auth when using cloudflare pages

--- apps/serp-wiki/nuxt.config.ts
+++ apps/serp-wiki/nuxt.config.ts
@@ -15,9 +15,6 @@ export default defineNuxtConfig({
     'nuxt-link-checker',
     '@nuxthub/core'
   ],
-  build: {
-    transpile: ['next-auth']
-  },
   auth: {
     baseURL: "https://staging.serp-wiki.pages.dev/api/auth",  // process.env.AUTH_ORIGIN,
     provider: {
