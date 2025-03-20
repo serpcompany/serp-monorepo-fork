b6f2cbc5cffb99ff12720f0e64ac63f9bc044969
devinschumacher <devin@serp.co>
2025-03-20 13:06:50 +0900

    remove upvote button from posts/ singles bc i dunno what/where to do with it

--- packages/ui/components/SinglePosts/Post.vue
+++ packages/ui/components/SinglePosts/Post.vue
@@ -1,14 +1,8 @@
 <template>
   <div>
-    <UpvoteButton
-      v-if="useAuth"
-      :id="data.slug"
-      module="posts"
-      :upvotes="data.upvotes"
-    />
     <section class="mb-8">
       <section-hero-one :title="data.title" />
-      <s-pill base-slug="posts/category" :items="data.categories" />
+      <s-pill base-slug="posts/category" :items="data.categories || []" />
     </section>
     <div class="items-end justify-between lg:flex">
       <div v-if="data?.author">Author: {{ data.author }}</div>
@@ -23,12 +17,11 @@
     <div class="mt-10">
       <h2 class="mb-4 text-2xl font-bold">Comments</h2>
       <CommentsContainer
-        v-if="useAuth"
-        :id="data.slug"
-        module="posts"
-        :comments="data.comments || []"
-        class="comments-github-style"
-      />
+                         v-if="useAuth"
+                         :id="data.slug"
+                         module="posts"
+                         :comments="data.comments || []"
+                         class="comments-github-style" />
     </div>
   </div>
 </template>
