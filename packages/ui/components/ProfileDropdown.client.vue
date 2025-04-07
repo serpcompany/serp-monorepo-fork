31b249e86cef1829f043fe86bac26a14c88595df
devinschumacher <devin@serp.co>
2025-04-07 17:07:56 +0900

    add more links to profile dropdown menu

--- packages/ui/components/ProfileDropdown.client.vue
+++ packages/ui/components/ProfileDropdown.client.vue
@@ -8,6 +8,11 @@
       {
         label: user?.value?.name,
         type: 'label'
+      },
+      {
+        label: 'Dashboard',
+        to: '/users/',
+        icon: 'i-lucide-user'
       }
     ],
     ...profileDropdownLinks,
