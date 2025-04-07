3073bbbd78961d93f01dd5af427327ade62033a0
devinschumacher <devin@serp.co>
2025-04-07 16:25:08 +0900

    fix updvote button text color on darkmode

--- packages/ui/components/UpvoteButton.vue
+++ packages/ui/components/UpvoteButton.vue
@@ -88,7 +88,7 @@
 <template>
   <div class="flex w-full items-center sm:w-auto">
     <UButton
-      class="bg-primary-50 disabled:bg-secondary-500 dark:disabled:bg-secondary-500 flex w-full items-center justify-center gap-2 rounded-full border border-neutral-300 px-4 py-2 font-medium text-neutral-700 transition-colors hover:bg-neutral-100 disabled:text-white disabled:opacity-90 sm:w-auto dark:border-neutral-600 dark:text-neutral-300 dark:hover:bg-neutral-100 dark:disabled:text-white"
+      class="bg-primary-50 disabled:bg-secondary-500 dark:disabled:bg-secondary-500 flex w-full items-center justify-center gap-2 rounded-full border border-neutral-300 px-4 py-2 font-medium text-neutral-700 transition-colors hover:bg-neutral-100 disabled:text-white disabled:opacity-90 sm:w-auto dark:border-neutral-600 dark:text-neutral-700 dark:hover:bg-neutral-100 dark:disabled:text-white"
       :disabled="loading"
       @click="upvote"
     >
