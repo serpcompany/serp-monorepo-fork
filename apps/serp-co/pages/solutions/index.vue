1bbdbc382d69a02b25b52819d3c30a48d79d5c4f
devinschumacher <devin@serp.co>
2025-04-22 13:34:01 -0700

    add working /solutions/ page from yaml

--- apps/serp-co/pages/solutions/index.vue
+++ apps/serp-co/pages/solutions/index.vue
@@ -1,3 +1,247 @@
+<script setup lang="ts">
+  const { data } = await useAsyncData('solutions', () =>
+    queryCollection('content').first()
+  );
+  if (!data.value) {
+    throw createError({
+      statusCode: 404,
+      statusMessage: 'Page not found',
+      fatal: true
+    });
+  }
+  const page = data.value.body;
+
+  useSeoMeta({
+    title: page.title,
+    ogTitle: page.title,
+    description: page.description,
+    ogDescription: page.description
+  });
+
+  definePageMeta({
+    layout: 'lander'
+  });
+</script>
+
 <template>
-  <div>service page index</div>
+  <div v-if="page" class="relative">
+    <div class="hidden lg:block">
+      <UColorModeImage
+        light="/images/light/line-1.svg"
+        dark="/images/dark/line-1.svg"
+        class="pointer-events-none absolute top-0 left-0 h-[650px] object-cover pb-10"
+      />
+    </div>
+
+    <UPageHero
+      :title="page.hero.title"
+      :description="page.hero.description"
+      :links="page.hero.links"
+      :ui="{ container: 'md:pt-18 lg:pt-20' }"
+    >
+      <template #title>
+        <MDC
+          :value="page.hero.title"
+          class="mx-auto max-w-3xl *:leading-11 sm:*:leading-19"
+        />
+      </template>
+    </UPageHero>
+
+    <UPageSection
+      v-for="(section, index) in page.sections"
+      :key="index"
+      v-bind="section"
+      orientation="horizontal"
+      :ui="{
+        container: 'lg:px-0 2xl:px-20 mx-0 max-w-none md:mr-10',
+        features: 'gap-0'
+      }"
+      reverse
+    >
+      <template #title>
+        <MDC :value="section.title" class="sm:*:leading-11" />
+      </template>
+      <img
+        :src="section.images.desktop"
+        :alt="section.title"
+        class="left-0 hidden w-full max-w-2xl lg:block 2xl:hidden"
+      />
+      <img
+        :src="section.images.mobile"
+        :alt="section.title"
+        class="block lg:hidden 2xl:block 2xl:w-full 2xl:max-w-2xl"
+      />
+    </UPageSection>
+
+    <USeparator :ui="{ border: 'border-(--ui-primary)/30' }" />
+    <UPageSection
+      id="features"
+      v-bind="page.features"
+      :ui="{
+        title: 'text-left @container relative flex',
+        description: 'text-left'
+      }"
+      class="relative overflow-hidden"
+    >
+      <div
+        class="absolute top-10 -left-10 z-10 size-[300px] rounded-full bg-(--ui-primary) opacity-30 blur-[200px]"
+      ></div>
+      <div
+        class="absolute -right-10 -bottom-10 z-10 size-[300px] rounded-full bg-(--ui-primary) opacity-30 blur-[200px]"
+      ></div>
+      <template #title>
+        <MDC :value="page.features.title" class="*:leading-9" />
+        <div class="hidden @min-[1020px]:block">
+          <UColorModeImage
+            light="/images/light/line-2.svg"
+            dark="/images/dark/line-2.svg"
+            class="absolute top-0 right-0 size-full translate-x-[70%] scale-95 transform"
+          />
+        </div>
+      </template>
+    </UPageSection>
+    <USeparator :ui="{ border: 'border-(--ui-primary)/30' }" />
+
+    <UPageSection
+      id="steps"
+      v-bind="page.steps"
+      class="relative overflow-hidden"
+    >
+      <template #headline>
+        <UColorModeImage
+          light="/images/light/line-3.svg"
+          dark="/images/dark/line-3.svg"
+          class="absolute -top-10 right-1/2 h-24 sm:top-0"
+        />
+      </template>
+      <template #title>
+        <MDC :value="page.steps.title" />
+      </template>
+
+      <template #features>
+        <UPageCard
+          v-for="(step, index) in page.steps.items"
+          :key="index"
+          class="group"
+          :ui="{ container: 'p-4 sm:p-4', title: 'flex items-center gap-1' }"
+        >
+          <UColorModeImage
+            v-if="step.image"
+            :light="step.image?.light"
+            :dark="step.image?.dark"
+            :alt="step.title"
+            class="size-full"
+          />
+
+          <div class="flex flex-col gap-2">
+            <span class="text-lg font-semibold">
+              {{ step.title }}
+            </span>
+            <span class="text-sm text-(--ui-text-muted)">
+              {{ step.description }}
+            </span>
+          </div>
+        </UPageCard>
+      </template>
+    </UPageSection>
+
+    <UPageSection
+      id="pricing"
+      class="mb-32 overflow-hidden"
+      v-bind="page.pricing"
+      :ui="{ title: 'text-left @container relative', description: 'text-left' }"
+    >
+      <template #title>
+        <MDC :value="page.pricing.title" />
+
+        <div class="hidden @min-[1120px]:block">
+          <UColorModeImage
+            light="/images/light/line-4.svg"
+            dark="/images/dark/line-4.svg"
+            class="absolute top-0 right-0 size-full translate-x-[60%] transform"
+          />
+        </div>
+      </template>
+
+      <UPricingPlans scale>
+        <UPricingPlan
+          v-for="(plan, index) in page.pricing.plans"
+          :key="index"
+          :title="plan.title"
+          :description="plan.description"
+          :price="plan.price"
+          :billing-period="plan.billing_period"
+          :billing-cycle="plan.billing_cycle"
+          :highlight="plan.highlight"
+          :scale="plan.highlight"
+          variant="soft"
+          :features="plan.features"
+          :button="plan.button"
+        />
+      </UPricingPlans>
+    </UPageSection>
+
+    <UPageSection id="testimonials" v-bind="page.testimonials">
+      <template #headline>
+        <UColorModeImage
+          light="/images/light/line-5.svg"
+          dark="/images/dark/line-5.svg"
+          class="absolute -top-10 right-1/2 h-24 sm:top-0"
+        />
+      </template>
+      <template #title>
+        <MDC :value="page.testimonials.title" />
+      </template>
+
+      <UContainer>
+        <UPageColumns class="xl:columns-3">
+          <UPageCard
+            v-for="(testimonial, index) in page.testimonials.items"
+            :key="index"
+            variant="subtle"
+            :description="testimonial.quote"
+            :ui="{
+              description:
+                'before:content-[open-quote] after:content-[close-quote]'
+            }"
+          >
+            <template #footer>
+              <UUser v-bind="testimonial.user" size="xl" />
+            </template>
+          </UPageCard>
+        </UPageColumns>
+      </UContainer>
+    </UPageSection>
+
+    <USeparator />
+
+    <UPageCTA
+      v-bind="page.cta"
+      variant="naked"
+      class="@container overflow-hidden"
+    >
+      <template #title>
+        <MDC :value="page.cta.title" />
+
+        <div class="@max-[1280px]:hidden">
+          <UColorModeImage
+            light="/images/light/line-6.svg"
+            dark="/images/dark/line-6.svg"
+            class="absolute -top-10 left-10 h-full sm:top-0"
+          />
+          <UColorModeImage
+            light="/images/light/line-7.svg"
+            dark="/images/dark/line-7.svg"
+            class="absolute right-0 bottom-0 h-full"
+          />
+        </div>
+      </template>
+
+      <div
+        class="absolute left-1/2 size-40 -translate-x-1/2 -translate-y-80 transform rounded-full blur-[250px] sm:size-50 dark:bg-(--ui-primary)"
+      ></div>
+
+      <LazyStarsBg />
+    </UPageCTA>
+  </div>
 </template>
