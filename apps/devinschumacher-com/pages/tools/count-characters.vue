9483a860d82e5be69bc87ee6a9922db366b55edf
devinschumacher <devin@serp.co>
2025-05-08 17:09:52 -0700

    kk

--- apps/devinschumacher-com/pages/tools/count-characters.vue
+++
@@ -1,3 +0,0 @@
-<template>
-  <SPageToolCountCharacters />
-</template>
--- apps/devinschumacher-com/pages/users/index.vue
+++
@@ -1,8 +0,0 @@
-<script setup lang="ts">
-  const router = useRouter();
-  router.replace({ path: '/users/dashboard/', replace: true });
-</script>
-
-<template>
-  <div></div>
-</template>
