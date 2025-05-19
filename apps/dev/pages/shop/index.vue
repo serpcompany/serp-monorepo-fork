c8739e55c9d7383a557b60add312d610d6308e05
Francis LaBounty <73464335+francislabountyjr@users.noreply.github.com>
2025-05-19 16:11:27 -0600

    remove sym links

---
+++ apps/dev/pages/shop/index.vue
@@ -0,0 +1,10 @@
+<template>
+  <SPagePostCollection
+    module="shop"
+    module-title="Shop"
+    base-slug="shop/best/"
+    seo-title="Shop"
+    :use-grid="true"
+    :no-categories="true"
+  />
+</template>
