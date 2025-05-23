356006162810521c626dedb102124aafafdd4b0f
Pedro Torres <pedro9031@gmail.com>
2025-05-23 01:04:58 -0400

    Refactor: Update SHero component template
    The component's template was updated to better structure the layout and
    conditional rendering of elements such as the headline, subheadline,
    search bar, and buttons. The changes include improved flexbox
    alignment, text styling, and conditional rendering using v-if and
    template tags for better organization and readability.

--- packages/ui/components/SHero.vue
+++ packages/ui/components/SHero.vue
@@ -15,44 +15,43 @@
 </script>
 
 <template>
-  <div>
-    <div class="py-20">
-      <div class="mx-auto max-w-5xl px-4 sm:px-6 lg:px-8">
-        <div class="items-center">
+  <div class="py-20">
+    <div class="mx-auto max-w-5xl px-4 sm:px-6 lg:px-8">
+      <div
+        class="flex flex-col items-center gap-4 text-center lg:items-start lg:text-left"
+      >
+        <h1 class="text-3xl font-bold text-pretty lg:text-5xl">
+          {{ headline }}
+        </h1>
+
+        <template v-if="subheadline">
+          <p class="text-muted max-w-2xl lg:text-xl">
+            {{ subheadline }}
+          </p>
+        </template>
+
+        <template v-if="showSearchBar && showButtons">
           <div
-            class="flex flex-col items-center text-center lg:items-start lg:text-left"
+            class="mt-4 flex w-full flex-col justify-center gap-2 sm:flex-row lg:justify-start"
           >
-            <h1 class="my-4 text-2xl font-bold text-pretty lg:text-5xl">
-              {{ headline }}
-            </h1>
-            <p
-              v-if="subheadline"
-              class="text-muted-foreground mb-8 max-w-2xl lg:text-xl"
-            >
-              {{ subheadline }}
-            </p>
-            <div
-              class="flex w-full flex-col justify-center gap-2 sm:flex-row lg:justify-start"
-            >
-              <div v-if="showSearchBar">
-                <!-- <SearchBar /> -->
-              </div>
-              <template v-if="showButtons">
-                <UButton
-                  label="Primary Button"
-                  class="w-full cursor-pointer rounded-none text-lg sm:w-auto"
-                  >Primary Button</UButton
-                >
-                <UButton
-                  variant="outline"
-                  class="w-full cursor-pointer rounded-none text-lg sm:w-auto"
-                >
-                  Buying Advice
-                </UButton>
-              </template>
+            <div v-if="showSearchBar">
+              <!-- <SearchBar /> -->
             </div>
+            <template v-if="showButtons">
+              <UButton
+                label="Primary Button"
+                class="w-full cursor-pointer rounded-none text-lg sm:w-auto"
+                >Primary Button</UButton
+              >
+              <UButton
+                variant="outline"
+                class="w-full cursor-pointer rounded-none text-lg sm:w-auto"
+              >
+                Buying Advice
+              </UButton>
+            </template>
           </div>
-        </div>
+        </template>
       </div>
     </div>
   </div>
