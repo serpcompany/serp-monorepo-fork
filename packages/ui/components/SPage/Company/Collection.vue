841cd84772ce0f303eebc570e66dd5bd4154590a
Pedro Torres <pedro9031@gmail.com>
2025-05-25 17:28:25 -0400

    Feat (UI): Refactor Company Collection component
    - Use `useAsyncData` for improved data fetching management.
    - Extract filtering and list rendering logic into dedicated components.

--- packages/ui/components/SPage/Company/Collection.vue
+++ packages/ui/components/SPage/Company/Collection.vue
@@ -1,35 +1,45 @@
 <script setup lang="ts">
-  const router = useRouter();
   const route = useRoute();
 
   const page = ref(Number(route.query.page) || 1);
-  const limit = ref(Number(route.query.limit) || 50);
 
-  // @ts-expect-error: Auto-imported from another layer
-  const categories = await useCompanyCategories();
+  const filters = computed(() => ({
+    page: page.value,
+    limit: Number(route.query.limit) || 50,
+    category: route.query.category as string,
+    q: route.query.q as string,
+    sort: route.query.sort as string
+  }));
 
-  // @ts-expect-error: Auto-imported from another layer
-  let data = await useCompanies(page.value, limit.value);
-  if (!data) {
-    router.push('/404');
-  }
-
-  watch([page, limit], async ([newPage, newLimit]) => {
-    const query = { ...route.query };
-    if (newPage !== 1) {
-      query.page = String(newPage);
-    } else {
-      delete query.page;
+  const { data, status } = await useAsyncData(
+    'companies',
+    () =>
+      useCompanies(
+        filters.value.page,
+        filters.value.limit,
+        filters.value.category,
+        filters.value.q,
+        filters.value.sort
+      ),
+    {
+      lazy: true,
+      watch: [filters]
     }
-    if (newLimit !== 50) {
-      query.limit = String(newLimit);
-    } else {
-      delete query.limit;
-    }
+  );
 
-    // @ts-expect-error: Auto-imported from another layer
-    data = await useCompanies(page.value, limit.value);
-    router.push({ query });
+  const { data: categories } = await useAsyncData(
+    'categories',
+    () => useCompanyCategories(),
+    {
+      default: () => []
+    }
+  );
+
+  const categoriesFilters = computed(() => {
+    return categories.value.map((category) => ({
+      label: category.name,
+      value: category.slug
+    }));
   });
 
   useSeoMeta({
@@ -45,24 +55,23 @@
       :show-search-bar="false"
       :show-buttons="false"
     />
-    <main class="space-y-20">
-      <div class="grid grid-cols-1 gap-4 lg:grid-cols-2">
-        <CompanyCard
-          v-for="company in data.companies"
-          :key="company.slug"
-          :company="company"
+    <main>
+      <div class="flex flex-col gap-y-6">
+        <CollectionFilters
+          :loading="status === 'pending'"
+          :categories="categoriesFilters"
+        />
+        <CompanyCardList
+          v-model:page="page"
+          :loading="status === 'pending'"
+          :items="data?.companies || []"
+          :pagination-limit="filters.limit"
+          :pagination-total="data?.pagination.totalItems"
         />
       </div>
-      <UPagination
-        v-model:page="page"
-        :total="data?.pagination?.totalItems"
-        :items-per-page="limit"
-        :sibling-count="3"
-        aria-label="pagination"
-        class="flex justify-center overflow-x-auto rounded-none"
-      />
+
       <SLinkHub
-        v-if="categories && categories.length"
+        v-if="categories.length"
         :categories="categories"
         headline="Categories"
         base-slug="products/best"
