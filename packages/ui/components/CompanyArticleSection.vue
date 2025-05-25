54543272c5a6ce0f1e18a263cbe2a0946b32ef97
Pedro Torres <pedro9031@gmail.com>
2025-05-25 17:23:41 -0400

    Feat (UI/Tests): Refine CompanyArticleSection styling
    Use CSS variable for background class.
    Remove max-w-5xl constraint from article content.
    Remove aria-labelledby attribute.

--- packages/ui/components/CompanyArticleSection.vue
+++ packages/ui/components/CompanyArticleSection.vue
@@ -5,12 +5,12 @@
 </script>
 
 <template>
-  <UPageSection title="Buying Advice" class="bg-white dark:bg-gray-900">
-    <article v-if="article" role="article" aria-labelledby="article-title">
-      <!-- eslint-disable-next-line vue/no-v-html -->
+  <UPageSection title="Buying Advice" class="bg-(--ui-bg)">
+    <article v-if="article">
+      <!-- eslint-disable vue/no-v-html -->
       <div
         role="document"
-        class="prose dark:prose-invert mx-auto max-w-5xl"
+        class="prose dark:prose-invert mx-auto max-w-full"
         v-html="article"
       ></div>
     </article>
