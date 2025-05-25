ca416728321af6bba201213bc10cfd02819ff2c2
Pedro Torres <pedro9031@gmail.com>
2025-05-25 04:31:03 -0400

    Feat (UI): Add Skeleton Components
    Adds skeleton components for CompanyCard and MCPServerCard.

---
+++ packages/ui/components/Skeleton/CompanyCard.vue
@@ -0,0 +1,39 @@
+<template>
+  <UCard
+    :ui="{
+      root: 'flex flex-col divide-dashed transition-all',
+      body: 'p-5 sm:p-5 flex-grow',
+      footer: `px-5 py-4 sm:px-5 bg-gray-50 dark:bg-black/10 rounded-b-lg`
+    }"
+    variant="outline"
+  >
+    <div class="flex flex-col gap-4">
+      <div class="flex flex-row gap-4">
+        <div class="bg-muted size-auto rounded-sm border border-(--ui-border)">
+          <USkeleton class="h-12 w-12 rounded-sm" />
+        </div>
+        <div class="flex flex-1 items-center gap-x-2">
+          <div class="flex flex-col gap-2">
+            <USkeleton class="h-7 w-32" />
+          </div>
+        </div>
+      </div>
+      <div class="flex flex-col gap-6">
+        <USkeleton class="h-3.5" />
+        <div class="inline-flex flex-wrap items-center gap-2">
+          <USkeleton
+            v-for="index in 3"
+            :key="index"
+            class="h-4 w-16 rounded-full"
+          />
+        </div>
+      </div>
+    </div>
+    <template #footer>
+      <div class="flex flex-row items-center gap-3">
+        <USkeleton class="h-7 w-20" />
+        <USkeleton class="ml-auto h-7 w-7" />
+      </div>
+    </template>
+  </UCard>
+</template>
---
+++ packages/ui/components/Skeleton/MCPServerCard.vue
@@ -0,0 +1,36 @@
+<template>
+  <UCard
+    :ui="{
+      root: 'flex flex-col divide-dashed transition-all',
+      body: 'p-5 sm:p-5 flex-grow',
+      footer:
+        'px-5 py-4 sm:px-5 sm:py-4 bg-gray-50 dark:bg-black/10 rounded-b-lg'
+    }"
+  >
+    <div class="flex flex-col gap-3">
+      <USkeleton class="h-6 w-48" />
+      <USkeleton class="h-3.5 w-64" />
+      <div class="flex flex-row flex-wrap items-center justify-start gap-1.5">
+        <USkeleton v-for="i in 3" :key="i" class="h-3.5 w-16" />
+      </div>
+    </div>
+    <template #footer>
+      <div class="flex flex-row items-center gap-3">
+        <div class="inline-flex items-center gap-1.5">
+          <UIcon name="lucide:star" class="size-4" />
+          <span class="text-muted text-xs">0</span>
+        </div>
+        <div class="inline-flex items-center gap-1.5">
+          <UIcon name="lucide:git-fork" class="size-4" />
+          <span class="text-muted text-xs">0</span>
+        </div>
+        <UButton
+          variant="ghost"
+          size="xs"
+          icon="lucide:external-link"
+          class="ml-auto"
+        />
+      </div>
+    </template>
+  </UCard>
+</template>
